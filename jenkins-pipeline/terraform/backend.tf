04dc646b95726dfc114d1fd473ea8b82ef16dfc6
kawaz971 <j.marignan@yahoo.fr>
2023-12-05 08:28:08 +0000

    maj S3

--- jenkins-pipeline/terraform/backend.tf
+++ jenkins-pipeline/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "primuslearning-app"
-    region = "us-east-1"
+    bucket = "projetfilrouge-web1"
+    region = "eu-west-3"
     key = "eks/terraform.tfstate"
   }
 }
